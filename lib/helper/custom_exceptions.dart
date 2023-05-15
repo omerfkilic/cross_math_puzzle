fd327d4cdb0523fcdbc4e3104d7307717269a683
Ömer Faruk Kılıç <omerf.kilic@outlook.com>
2023-05-15 17:03:11 +0300

    ArithmeticOperatorTypes.subtraction and its logics
    Fixed some bugs and added new TODO's :)

--- lib/helper/custom_exceptions.dart
+++ lib/helper/custom_exceptions.dart
@@ -7,6 +7,15 @@ class AddOperationTimedOutException implements Exception {
   }
 }
 
+class FillBoxesTimedOutException implements Exception {
+  final String message;
+  FillBoxesTimedOutException(this.message);
+  @override
+  String toString() {
+    return "Exception: $message";
+  }
+}
+
 class ThereIsNotAnyAvailableMathOperationToFillException implements Exception {
   final String message;
   ThereIsNotAnyAvailableMathOperationToFillException(this.message);
